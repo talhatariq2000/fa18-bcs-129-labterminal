fffd4a33e186334cf1461b9cd619bd53491766f3
talhatariq2000 <76400023+talhatariq2000@users.noreply.github.com>
2021-06-25 17:12:35 +0500

    Update addmatchajax.js

--- addmatchajax.js
+++ addmatchajax.js
@@ -1,24 +1,46 @@
 $(function(){
     loadteams();
+    $("#kka").click(kka);
+    $("#pza").click(pza);
+    $("#qga").click(qga);
+    $("#iua").click(iua);
+    $("#msa").click(msa);
+    $("#lqa").click(lqa);
+
+    $("#kkb").click(kkb);
+    $("#pzb").click(pzb);
+    $("#qgb").click(qgb);
+    $("#iub").click(iub);
+    $("#msb").click(msb);
+    $("#lqb").click(lqb);
 });
 
+function kka() {
+    $("#teama").append("Karachi Kings");
+}
+function pza() {
+    $("#teama").val("Peshawar Zalmi");
+}
+function qga() {
+    $("#teama").val("Quetta Gladiators");
+}
+function iua() {
+    $("#teama").val("Islamabad United");
+}
+function lqa() {
+    $("#teama").val("Lahore Qalandars");
+}
+function kka() {
+    $("#teama").val("");
+}
+
+function handleadda() {
+    var newa = $(".adda").id();
+
+    $("#todos").append();
+}
 
 function loadteams() {
-    $.ajax({
-        url:"http://localhost:3001/api/teams/",
-        method:"GET",
-        success:function(response){
-            console.log(response);
-            var teams = $("#teams");
-            teams.empty();
-            for(var i=0;i<response.length;i++)
-            {
-                    var team = response[i];
-                    teams.append(`
-                        <button id="${team._id}">${team.name}</button>
-                    `);
-            }
-        }
-    });
+    
 }
 
